eab81e1c9b0989e059e91cac2ebcf29f73ddf3db
Pumpkin <wishsmp1002@gmail.com>
2025-12-29 05:34:00 +1100

    Initial commit

---
+++ src/main/java/net/saturn/betterCombatLogger/BetterCombatLogger.java
@@ -0,0 +1,17 @@
+package net.saturn.betterCombatLogger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BetterCombatLogger extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
